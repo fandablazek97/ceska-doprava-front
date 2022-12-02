e38db109370f892bb237daac38a30b57db3d53ba
ezyxels <ezyxels@gmail.com>
2022-12-02 03:15:17 +0100

    richtext

--- src/components/zajezd/Information.tsx
+++ src/components/zajezd/Information.tsx
@@ -1,6 +1,7 @@
 import Heading from "@components/bricks/Heading";
 import Wrapper from "@components/bricks/Wrapper";
 import Image from "next/image";
+import ReactMarkdown from 'react-markdown'
 
 type Props = {
   text?: string;
@@ -27,59 +28,84 @@ export default function Information({
       id="informace"
       className="flex flex-col gap-y-10 mb-16"
     >
-      <div className={`${text === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Popis</Heading>
-        <p className="text-sm my-6">{text}</p>
-      </div>
-      <div className={`${information === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Důležité informace</Heading>
-        <p className="text-sm my-6">{information}</p>
-      </div>
-      <div className={`${transport === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Doprava</Heading>
-        <p className="text-sm my-6">{transport}</p>
-      </div>
-      {departurePoints !== null && departurePoints.length !== 0 &&
-      <div>
-        <Heading level={2} size={"base"}>Odjezdová místa</Heading>
-          {departurePoints.map((point: any, i: any) => (
-            <div className="flex flex-col mt-5 gap-y-3">
-              <Heading level={3} size={"sm"}>{point.attributes.nazev} trasa</Heading>
-              {point.attributes.mesta.map((city: any, i: number) => (
-                i + 1 === point.attributes.mesta.length ? city.nazevMesta : city.nazevMesta + ", "
-              ))}
-              {point.attributes.obrazek.data !== null &&
-                <div className="w-full aspect-square max-h-[400px] relative rounded-md overflow-hidden">
-                  <Image
-                      src={point.attributes.obrazek.data.attributes.url}
-                      layout="fill"
-                      objectFit="contain"
-                      sizes="(max-width: 768px) 50vw,
-                      (max-width: 1200px) 50vw,
-                      50vw"
-                    />
-                </div>
-              }
+      {text && 
+        <div>
+          <Heading level={2} size={"base"}>Popis</Heading>
+          <div className="[&>p]:text-sm my-6">
+            <ReactMarkdown>{text}</ReactMarkdown>
             </div>
-           ))}
-      </div>
+        </div>
       }
-      <div className={` ${programme === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Program</Heading>
-        <p className="text-sm my-6">{programme}</p>
-      </div>
-      <div className={`${comment === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Poznámka k zájezdu</Heading>
-        <p className="text-sm my-6">{comment}</p>
-      </div>
-      <div className={`${comment === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Poznámka k zájezdu</Heading>
-        <p className="text-sm my-6">{comment}</p>
-      </div>
-      <div className={`${organizer === null ? "hidden" : "block"}`}>
-        <Heading level={2} size={"base"}>Pořadatel</Heading>
-        <p className="text-lg my-6">{organizer}</p>
-      </div>
+
+      {information && 
+        <div>
+          <Heading level={2} size={"base"}>Důležité informace</Heading>
+          <div className="[&>p]:text-sm my-6">
+            <ReactMarkdown>{information}</ReactMarkdown>
+            </div>
+        </div>
+      }
+
+      {transport && 
+        <div>
+          <Heading level={2} size={"base"}>Doprava</Heading>
+          <div className="[&>p]:text-sm my-6">
+            <ReactMarkdown>{transport}</ReactMarkdown>
+            </div>
+        </div>
+      }
+
+      {departurePoints && departurePoints.length !== 0 &&
+        <div>
+          <Heading level={2} size={"base"}>Odjezdová místa</Heading>
+            {departurePoints.map((point: any, key: any) => (
+              <div className="flex flex-col mt-5 gap-y-3" key={key}>
+                <Heading level={3} size={"sm"}>{point.attributes.nazev} trasa</Heading>
+                {point.attributes.mesta.map((city: any, i: number) => (
+                  i + 1 === point.attributes.mesta.length ? city.nazevMesta : city.nazevMesta + ", "
+                ))}
+                {point.attributes.obrazek.data !== null &&
+                  <div className="w-full aspect-square max-h-[400px] relative rounded-md overflow-hidden">
+                    <Image
+                        src={point.attributes.obrazek.data.attributes.url}
+                        layout="fill"
+                        objectFit="contain"
+                        sizes="(max-width: 768px) 50vw,
+                        (max-width: 1200px) 50vw,
+                        50vw"
+                      />
+                  </div>
+                }
+              </div>
+            ))}
+        </div>
+      }
+
+      {programme && 
+        <div>
+          <Heading level={2} size={"base"}>Program</Heading>
+          <div className="[&>p]:text-sm my-6">
+            <ReactMarkdown>{programme}</ReactMarkdown>
+            </div>
+        </div>
+      }
+
+      {comment && 
+        <div>
+          <Heading level={2} size={"base"}>Poznámka k zájezdu</Heading>
+          <div className="[&>p]:text-sm my-6">
+            <ReactMarkdown>{comment}</ReactMarkdown>
+            </div>
+        </div>
+      }
+
+      {organizer &&
+        <div>
+          <Heading level={2} size={"base"}>Pořadatel</Heading>
+          <p className="text-lg my-6">{organizer}</p>
+        </div>
+      }
+
       <p className="mt-10 font-bold">ZMĚNA PROGRAMU VYHRAZENA</p>
     </Wrapper>
   )
