b560f036a997db11c66091361e94df542024e92f
ezyxels <ezyxels@gmail.com>
2023-03-07 11:59:59 +0100

    href="#filter"  změněno na scroll:false

--- src/components/zajezdy/ContentAndFilter.tsx
+++ src/components/zajezdy/ContentAndFilter.tsx
@@ -39,7 +39,7 @@ export default function ContentAndFilter({category, zajezdData} : {category: str
                     ${elem[0] === category ? "bg-gray-200" : "hover:bg-gray-100"
                   }`}
               >
-                <Link href={`${elem[0]}#filter`}><a>
+                <Link href={`${elem[0]}`} scroll={false}><a>
                 {elem[1]}</a></Link>
               </li>
             ))}
