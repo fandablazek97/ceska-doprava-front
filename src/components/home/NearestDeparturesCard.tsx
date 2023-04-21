70196763b49a211eb3b7c27e9aeefcb6ff597d87
ezyxels <ezyxels@gmail.com>
2023-04-21 10:06:39 +0200

    main fix

--- src/components/home/NearestDeparturesCard.tsx
+++ src/components/home/NearestDeparturesCard.tsx
@@ -33,54 +33,50 @@ export default function NearestDeparturesCard({
     dateFrom: string;
     dateTo: string;
   }>({ dateFrom: "2023-12-31", dateTo: "2023-12-31" });
+  
+  let dateFrom: string = "2024-12-31";
+  let dateTo: string | null = "2024-12-31";
+  let today = new Date().toISOString().slice(0, 10);
 
-  useEffect(() => {
-    let tempDateFrom = "2023-12-31";
-    let tempDateTo = "2023-12-31";
-
-    !fake && dateAndPrice &&
-      dateAndPrice.map((entry, index) => {
-        if (index + 1 === dateAndPrice!.length) {
-          if (
-            new Date(entry.datumOd).getTime() >= new Date().getTime() &&
-            new Date(entry.datumOd).getTime() < new Date(tempDateFrom).getTime()
-          ) {
-            if (entry.datumDo === null) {
-              setChangables({
-                dateFrom: changeDateType(entry.datumOd),
-                dateTo: "none",
-              });
-            } else {
-              setChangables({
-                dateFrom: changeDateType(entry.datumOd),
-                dateTo: changeDateType(entry.datumDo),
-              });
-            }
-          } else {
-            if (entry.datumDo === null) {
-              setChangables({
-                dateFrom: changeDateType(tempDateFrom),
-                dateTo: "none",
-              });
-            } else {
-              setChangables({
-                dateFrom: changeDateType(tempDateFrom),
-                dateTo: changeDateType(tempDateTo),
-              });
-            }
-          }
+  !fake && dateAndPrice && dateAndPrice.map((entry, index) => {
+    if (index + 1 === dateAndPrice.length) {
+      if (
+        new Date(entry.datumOd).getTime() >=
+          new Date(today).getTime() &&
+        new Date(entry.datumOd).getTime() < new Date(dateFrom).getTime()
+      ) {
+        if (entry.datumDo === null) {
+          dateFrom = changeDateType(entry.datumOd);
+          dateTo = "none";
         } else {
-          if (
-            new Date(entry.datumOd).getTime() >= new Date().getTime() &&
-            new Date(entry.datumOd).getTime() < new Date(tempDateFrom).getTime()
-          ) {
-            tempDateFrom = entry.datumOd;
-            tempDateTo = entry.datumDo;
-          }
+          dateFrom = changeDateType(entry.datumOd);
+          dateTo = changeDateType(entry.datumDo);
         }
-      });
-  }, []);
-
+      } else {
+        if (entry.datumDo === null) {
+          dateFrom = changeDateType(dateFrom);
+          dateTo = "none";
+        } else {
+          dateFrom = changeDateType(dateFrom);
+          if(dateTo) dateTo = changeDateType(dateTo);
+        }
+      }
+    } else {
+      if (
+        new Date(entry.datumOd).getTime() >=
+          new Date(today).getTime() &&
+        new Date(entry.datumOd).getTime() < new Date(dateFrom).getTime()
+      ) {
+        if(entry.datumOd === entry.datumDo){
+          dateTo = null;
+        }
+        else{
+          dateTo = entry.datumDo;
+        }
+        dateFrom = entry.datumOd;
+      }
+    }
+  });
   function changeDateType(date: string) {
     var newDate = date.split("-")[2] + "." + date.split("-")[1] + ".";
     return newDate;
@@ -128,10 +124,13 @@ export default function NearestDeparturesCard({
           <span
             className={`block ${fake ? "h-5 w-28 bg-gray-200" : "mt-auto"}`}
           >
-            {!fake &&
-              (changeables.dateTo === "none"
-                ? changeables.dateFrom
-                : changeables.dateFrom + "-" + changeables.dateTo)}
+            {!fake ? (dateTo === "none" || !dateTo || dateFrom === dateTo)
+                ? 
+                  dateFrom
+                  :
+                    dateFrom + " - " + dateTo
+                    : null
+              }
           </span>
         </div>
       </a>
