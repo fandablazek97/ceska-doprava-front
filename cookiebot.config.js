d8da9b34f1b3c7c5bd54399ce3e4c10c5c6371a9
ezyxels <ezyxels@gmail.com>
2024-12-10 12:06:03 +0100

    CookieBot, knihovny na nejnovější

---
+++ cookiebot.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  cbid: "d1b10f88-5121-448e-93b9-e4b875b25682",
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,24 @@
+import CookieBotDeclaration from "@components/CookieBotDeclaration";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { cbid } from "../../cookiebot.config.js";
 
 export default function Document() {
   return (
     <Html lang="cs">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid={cbid}
+          data-blockingmode="auto"
+          type="text/javascript"
+        />
+      </Head>
       <body>
         <Main />
+        <CookieBotDeclaration />
         <NextScript />
       </body>
     </Html>
